991aa93a5298ea13ed71975d92983dc92c11a563
sam-bao <sgarman@baosystems.com>
2019-02-18 14:35:58 +0100

    interim committ

---
+++ R/SiteDistMain.R
@@ -0,0 +1,24 @@
+devtools::install(pkg = "/Users/sam/Documents/GitHub/data-pack-commons",
+                  build = TRUE,
+                  upgrade = FALSE)
+
+# require(datapackcommons)
+# require(tidyverse)
+# require(jsonlite)
+# require(lubridate)
+# require(rlang)
+# require(assertthat)
+# require(foreach)
+# # require(rlist)
+# 
+# DistributeToSites <- function( , ){
+#   
+# }
+#   
+#   
+#   
+#   
+#   
+# require(datapackcommons)
+# 
+# DistributeToSites(datapackcommons::)
