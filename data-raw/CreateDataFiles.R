3d4795ced311f747340a5523745310a32a351ec8
sam-bao <sgarman@baosystems.com>
2019-02-18 13:14:50 +0100

    CreateDataFiles.R - add Map19Tto20T including validation

--- data-raw/CreateDataFiles.R
+++ data-raw/CreateDataFiles.R
@@ -68,6 +68,26 @@ ValidateDataRequired <- function(data_required, base_url){
   data_required %>% ValidateDimItems("B.add_dim_1_uid", "B.add_dim_1_items", "B.add_dim_1_items_uid", base_url)
 }
 
+ValidateMap19Tto20T <- function(Map19Tto20T, dim_item_sets, base_url){
+  
+  Map19Tto20T %>% mutate(dim_uid_colname = "LxhLO68FcXm") %>% 
+  ValidateDimItems("dim_uid_colname", "technical_area", "technical_area_uid", base_url) 
+  
+  Map19Tto20T %>% mutate(dim_uid_colname = "lD2x0c8kywj") %>% 
+    ValidateDimItems("dim_uid_colname", "num_or_den", "num_or_den_uid", base_url) 
+      
+  Map19Tto20T %>% mutate(dim_uid_colname = "HWPJnUTMjEq") %>% 
+    ValidateDimItems("dim_uid_colname", "disagg_type", "disagg_type_uid", base_url) 
+  
+# chack for matching model sets in Dimension item sets
+  c(Map19Tto20T$age_set, Map19Tto20T$sex_set, Map19Tto20T$kp_set, Map19Tto20T$other_disagg) %>% 
+    na.omit() %>% 
+    {. %in% dim_item_sets$model_sets} %>% 
+    all() %>% 
+    assertthat::assert_that()
+
+  }
+
 datapackcommons::DHISLogin("/users/sam/.secrets/prod.json")
 base_url <- getOption("baseurl")
 wd <- getwd()
@@ -106,10 +126,17 @@ data_required_test <-
                   na = c("NA")) %>%
   dplyr::mutate(B.kp_set = NA_character_) %>% select(-data_pack_type)
 
+Map19Tto20T <- 
+  readr::read_csv("./data-raw/site_distribution_configuration/19Tto20TMap.csv", 
+                  col_types = readr::cols(.default = "c"),
+                  na = c("NA")) 
 
+ValidateMap19Tto20T(Map19Tto20T, dim_item_sets, base_url)
 
 usethis::use_data(dim_item_sets, overwrite = TRUE, compress = "gzip")
 usethis::use_data(dim_item_sets_test, overwrite = TRUE, compress = "gzip")
 usethis::use_data(data_required, overwrite = TRUE, compress = "gzip")
 usethis::use_data(data_required_test, overwrite = TRUE, compress = "gzip")
+usethis::use_data(Map19Tto20T, overwrite = TRUE, compress = "gzip")
+
 setwd(wd)
