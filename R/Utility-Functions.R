990d921c4eeb97fccb27c49d708041d9f4b90c5c
Siddharth Dixit <sdixit@baosystems.com>
2019-02-22 09:41:55 -0500

    Initial changes before demo

--- R/Utility-Functions.R
+++ R/Utility-Functions.R
@@ -60,3 +60,64 @@ FormatForApi_Dimensions <- function(data, type_col, dim_id_col, item_id_col){
      .[[1]] %>% 
      paste0(collapse="&")
 }
+
+#' @export
+#' @title RenameDimensionColumns(data, type)
+#' 
+#' @description Takes columns from data with specified prefixes and stacks them based on the unprefixed
+#' portion of the name. Columns not containing one of the prefixes are excluded in returned data. 
+#' @param data dataframe - contains data to stack 
+#' @param prefixes string vector - list of prefixes to include in column selection and stacking
+#' @return  tibble with superset of columns without prefixes in column names
+#'
+RenameDimensionColumns <- function(data, type){
+  data %>% dplyr::rename(!!paste0(type,"_dim_uid") := dim_uid,
+                         !!paste0(type,"_dim_name") := dim_name,
+                         !!paste0(type,"_dim_cop_type") := dim_cop_type,
+                         !!paste0(type,"_dim_item_name") := dim_item_name,
+                         !!paste0(type,"_option_name") := option_name,
+                         !!paste0(type,"_option_uid") := option_uid,
+                         !!paste0(type,"_sort_order") := sort_order,
+                         !!paste0(type,"_weight") := weight,
+                         !!paste0(type,"_model_sets") := model_sets) %>% return()
+}
+
+#' @export
+#' @title MapDimToOptions(data, items_to_options, allocate)
+#' 
+#' @description Takes columns from data with specified prefixes and stacks them based on the unprefixed
+#' portion of the name. Columns not containing one of the prefixes are excluded in returned data. 
+#' @param data dataframe - contains data to stack 
+#' @param prefixes string vector - list of prefixes to include in column selection and stacking
+#' @return  tibble with superset of columns without prefixes in column names
+#'
+MapDimToOptions <- function(data, items_to_options, allocate){
+  
+  if(NROW(items_to_options) == 0){
+    return(data)
+  }
+  
+  dimension_uid <- unique(items_to_options$dim_uid)
+  cop_category <- unique(items_to_options$dim_cop_type)
+  assertthat::assert_that(NROW(dimension_uid) == 1, NROW(cop_category) == 1)
+  
+  if(is.na(dimension_uid)){
+    # We are in a scenario of distributing to category options in the absence of a source dimension
+    # so we need cartesian product of data with item_to_dim entries
+    joined_data <- tidyr::crossing(data, items_to_options)
+  } else {
+    dim_name <-  items_to_options[[1,"dim_name"]]
+    joined_data <- data %>%
+      left_join(items_to_options, by = setNames("dim_item_uid", dim_name))
+  }
+  
+  if(allocate == "distribute"){
+    joined_data %>%
+      mutate(Value = Value * weight) %>%
+      RenameDimensionColumns(stringr::str_remove(cop_category, "_set"))
+  } else{
+    joined_data %>%
+      RenameDimensionColumns(stringr::str_remove(cop_category, "_set"))
+  }
+}
+
