49c901aa2861c6613631d44a10d8511cf087ac6d
Siddharth Dixit <sdixit@baosystems.com>
2019-03-26 02:37:30 -0400

    Test changes

--- tests/testthat/test-analytics.R
+++ tests/testthat/test-analytics.R
@@ -186,7 +186,7 @@ httptest::stop_mocking()
 })
 
 # This method is not yet exported
-test_that("TransformAnalyticsOutput_SiteTool", {
+testthat::test_that("TransformAnalyticsOutput_SiteTool", {
   # Add sample data at top of the file so its reusable
   # OVC Serv sample data, aggregates two ages together, combine 18-24 and 25+ ages, look at dataset from mapdimtooptions test
   dimensions_sample <- tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
@@ -202,84 +202,61 @@ test_that("TransformAnalyticsOutput_SiteTool", {
   # J5jldMd8OHv = Facility Type, uYxK4wmcPqA = CHP, EYbopBOJWsW = MCHP
   datapackcommons::DHISLogin_Play("2.29")
   analytics_data <- datapackcommons::GetData_Analytics(dimensions_sample, "https://play.dhis2.org/2.29/")$results
-  # Sample analytics_data
-  # structure(list(`Facility Type` = c("uYxK4wmcPqA", "uYxK4wmcPqA", 
-  #                                    "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", 
-  #                                    "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", 
-  #                                    "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", "uYxK4wmcPqA", 
-  #                                    "uYxK4wmcPqA", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", 
-  #                                    "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", 
-  #                                    "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", 
-  #                                    "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW", "EYbopBOJWsW"), 
-  #                `Organisation unit` = c("fdc6uOvgoji", "fdc6uOvgoji", "lc3eMKXaEfw", 
-  #                                        "lc3eMKXaEfw", "jUb8gELQApl", "jUb8gELQApl", "kJq2mPyFEHo", 
-  #                                        "kJq2mPyFEHo", "qhqAxPSTUXp", "qhqAxPSTUXp", "Vth0fbpFcsO", 
-  #                                        "Vth0fbpFcsO", "jmIPBj66vD6", "jmIPBj66vD6", "bL4ooGhyHRQ", 
-  #                                        "bL4ooGhyHRQ", "at6UHUQatSo", "at6UHUQatSo", "fdc6uOvgoji", 
-  #                                        "fdc6uOvgoji", "lc3eMKXaEfw", "lc3eMKXaEfw", "PMa2VCrupOd", 
-  #                                        "PMa2VCrupOd", "kJq2mPyFEHo", "kJq2mPyFEHo", "qhqAxPSTUXp", 
-  #                                        "qhqAxPSTUXp", "Vth0fbpFcsO", "Vth0fbpFcsO", "jmIPBj66vD6", 
-  #                                        "jmIPBj66vD6", "bL4ooGhyHRQ", "bL4ooGhyHRQ", "at6UHUQatSo", 
-  #                                        "at6UHUQatSo"), `HIV age` = c("UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr", "UOqJW6HPvvL", "WAl0OCcIYxr", 
-  #                                                                      "UOqJW6HPvvL", "WAl0OCcIYxr"), Value = c(0, 0, 0.96, 0.44, 
-  #                                                                                                               0, 0, 1.7, 1.1, 0, 0, 4.8, 3.9, 0, 0, 0.66, 0, 15.5, 9.5, 
-  #                                                                                                               0, 1.7, 4.8, 5.7, 0, 0, 1, 2.1, 0.6, 0.85, 3, 2.6, 3.4, 0, 
-  #                                                                                                               0, 0, 16.2, 15.9), ou_hierarchy = list(c("ImspTQPwCqd", "fdc6uOvgoji"
-  #                                                                                                               ), c("ImspTQPwCqd", "fdc6uOvgoji"), c("ImspTQPwCqd", "lc3eMKXaEfw"
-  #                                                                                                               ), c("ImspTQPwCqd", "lc3eMKXaEfw"), c("ImspTQPwCqd", "jUb8gELQApl"
-  #                                                                                                               ), c("ImspTQPwCqd", "jUb8gELQApl"), c("ImspTQPwCqd", "kJq2mPyFEHo"
-  #                                                                                                               ), c("ImspTQPwCqd", "kJq2mPyFEHo"), c("ImspTQPwCqd", "qhqAxPSTUXp"
-  #                                                                                                               ), c("ImspTQPwCqd", "qhqAxPSTUXp"), c("ImspTQPwCqd", "Vth0fbpFcsO"
-  #                                                                                                               ), c("ImspTQPwCqd", "Vth0fbpFcsO"), c("ImspTQPwCqd", "jmIPBj66vD6"
-  #                                                                                                               ), c("ImspTQPwCqd", "jmIPBj66vD6"), c("ImspTQPwCqd", "bL4ooGhyHRQ"
-  #                                                                                                               ), c("ImspTQPwCqd", "bL4ooGhyHRQ"), c("ImspTQPwCqd", "at6UHUQatSo"
-  #                                                                                                               ), c("ImspTQPwCqd", "at6UHUQatSo"), c("ImspTQPwCqd", "fdc6uOvgoji"
-  #                                                                                                               ), c("ImspTQPwCqd", "fdc6uOvgoji"), c("ImspTQPwCqd", "lc3eMKXaEfw"
-  #                                                                                                               ), c("ImspTQPwCqd", "lc3eMKXaEfw"), c("ImspTQPwCqd", "PMa2VCrupOd"
-  #                                                                                                               ), c("ImspTQPwCqd", "PMa2VCrupOd"), c("ImspTQPwCqd", "kJq2mPyFEHo"
-  #                                                                                                               ), c("ImspTQPwCqd", "kJq2mPyFEHo"), c("ImspTQPwCqd", "qhqAxPSTUXp"
-  #                                                                                                               ), c("ImspTQPwCqd", "qhqAxPSTUXp"), c("ImspTQPwCqd", "Vth0fbpFcsO"
-  #                                                                                                               ), c("ImspTQPwCqd", "Vth0fbpFcsO"), c("ImspTQPwCqd", "jmIPBj66vD6"
-  #                                                                                                               ), c("ImspTQPwCqd", "jmIPBj66vD6"), c("ImspTQPwCqd", "bL4ooGhyHRQ"
-  #                                                                                                               ), c("ImspTQPwCqd", "bL4ooGhyHRQ"), c("ImspTQPwCqd", "at6UHUQatSo"
-  #                                                                                                               ), c("ImspTQPwCqd", "at6UHUQatSo"))), class = c("tbl_df", 
-  #                                                                                                                                                               "tbl", "data.frame"), row.names = c(NA, -36L))
+  
   data_element_map = datapackcommons::Map19Tto20T[36,]
   # Sample data_element_map
-  # structure(list(indicatorCode_fy20_cop = "TB_ART.N.Age/Sex/NewExistingART/HIVStatus.20T.Already", 
-  #                `Technical Area_fy20_cop` = "TB_ART", `Numerator / Denominator_fy20_cop` = "N", 
-  #                `Disagregation Type_fy20_cop` = "Age/Sex/NewExistingART/HIVStatus", 
-  #                Other_fy20_cop = "Already", dx = "DE_GROUP-zhdJiWlPvCz", 
-  #                technical_area = "TB_ART", technical_area_uid = "Z6TU9Os82Yw", 
-  #                num_or_den = "Numerator", num_or_den_uid = "Som9NRMQqV7", 
-  #                disagg_type = "Age Aggregated/Sex/HIVStatus", disagg_type_uid = "h0pvSVe1TYf", 
-  #                pe = "2018Oct", age_set = "<15/>15.d", sex_set = "F/M", kp_set = NA_character_, 
-  #                other_disagg = NA_character_, allocate = "distribute"), class = c("tbl_df", 
-  #                                                                                  "tbl", "data.frame"), row.names = c(NA, -1L))
+  # structure(list(indicatorCode_fy20_cop = "OVC_SERV.N.Age/Sex/ProgramStatus.20T.Active", 
+  #                `Technical Area_fy20_cop` = "OVC_SERV", `Numerator / Denominator_fy20_cop` = "N", 
+  #                `Disagregation Type_fy20_cop` = "Age/Sex/ProgramStatus", 
+  #                Other_fy20_cop = "Active", dx = "DE_GROUP-zhdJiWlPvCz", technical_area = "OVC_SERV", 
+  #                technical_area_uid = "RxyNwEV3oQf", num_or_den = "Numerator", 
+  #                num_or_den_uid = "Som9NRMQqV7", disagg_type = "Age/Sex", 
+  #                disagg_type_uid = "Qbz6SrpmJ1y", pe = "2018Oct", age_set = "<1-18+", 
+  #                sex_set = "F/M/U", kp_set = NA_character_, other_disagg = NA_character_, 
+  #                allocate = "distribute"), class = c("tbl_df", "tbl", "data.frame"
+  #                ), row.names = c(NA, -1L))
+                                                                                                                                                                                                                                                                                                                                                                                                                    
+  sample_data_1to9 <- structure(list(`Age: Cascade Age bands` = "egW0hBcZeD2", `Disaggregation Type` = "Qbz6SrpmJ1y", 
+                                     `Cascade sex` = "Gxcf2DK8vNc", `Numerator / Denominator` = "Som9NRMQqV7", 
+                                     `Technical Area` = "RxyNwEV3oQf", `Organisation unit` = "Test1", 
+                                     `Funding Mechanism` = "Test2", Value = 333, ou_hierarchy = list(
+                                       c("Test3", "Test4", "Test5", "Test1"
+                                       ))), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -1L))
   
-  analytics_data <- structure(list(`Age: Cascade Age bands` = c("noEF3Mx7aBu", "Z8MTaDxRBP6", 
-                                                                "noEF3Mx7aBu"), `Disaggregation Type` = c("Qbz6SrpmJ1y", "Qbz6SrpmJ1y", 
-                                                                                                          "Qbz6SrpmJ1y"), `Cascade sex` = c("hDBPKTjUPDm", "Gxcf2DK8vNc", 
-                                                                                                                                            "ZOYVg7Hosni"), `Numerator / Denominator` = c("Som9NRMQqV7", 
-                                                                                                                                                                                          "Som9NRMQqV7", "Som9NRMQqV7"), `Technical Area` = c("RxyNwEV3oQf", 
-                                                                                                                                                                                                                                              "RxyNwEV3oQf", "RxyNwEV3oQf"), `Organisation unit` = c("qYzGABaWyCf", 
-                                                                                                                                                                                                                                                                                                     "Z3IDOaDDkwG", "nxGb6sd7p7D"), `Funding Mechanism` = c("eX9p46QTgjL", 
-                                                                                                                                                                                                                                                                                                                                                            "Y8JtX87RYYP", "eX9p46QTgjL"), Value = c(108, 807, 120), ou_hierarchy = list(
-                                                                                                                                                                                                                                                                                                                                                              c("ybg3MO3hcf4", "KSkooYTy8FB", "V0qMZH29CtN", "qYzGABaWyCf"
-                                                                                                                                                                                                                                                                                                                                                              ), c("ybg3MO3hcf4", "KSkooYTy8FB", "V0qMZH29CtN", "Z3IDOaDDkwG"
-                                                                                                                                                                                                                                                                                                                                                              ), c("ybg3MO3hcf4", "KSkooYTy8FB", "V0qMZH29CtN", "nxGb6sd7p7D"
-                                                                                                                                                                                                                                                                                                                                                              ))), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, 
-                                                                                                                                                                                                                                                                                                                                                                                                                           -3L))
+  sample_data_18to24 <- structure(list(`Age: Cascade Age bands` = "N0PwGN3UKWx", `Disaggregation Type` = "Qbz6SrpmJ1y", 
+                                       `Cascade sex` = "hDBPKTjUPDm", `Numerator / Denominator` = "Som9NRMQqV7", 
+                                       `Technical Area` = "RxyNwEV3oQf", `Organisation unit` = "Test1", 
+                                       `Funding Mechanism` = "Test2", Value = 333, ou_hierarchy = list(
+                                         c("Test3", "Test4", "Test5", "Test1"
+                                         ))), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -1L))
+  
+  sample_data_25_plus <- structure(list(`Age: Cascade Age bands` = "pk98FEsOJcz", `Disaggregation Type` = "Qbz6SrpmJ1y", 
+                                        `Cascade sex` = "hDBPKTjUPDm", `Numerator / Denominator` = "Som9NRMQqV7", 
+                                        `Technical Area` = "RxyNwEV3oQf", `Organisation unit` = "Test1", 
+                                        `Funding Mechanism` = "Test2", Value = 333, ou_hierarchy = list(
+                                          c("Test3", "Test4", "Test5", "Test1"
+                                          ))), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, -1L))
   
   # MapDimToOptions not found (so prefaced it in SiteDistMain)
-  test_output <- TransformAnalyticsOutput_SiteTool(analytics_data, datapackcommons::dim_item_sets,
+  test_output <- TransformAnalyticsOutput_SiteTool(sample_data_1to9, datapackcommons::dim_item_sets,
                                                                     data_element_map, 4)
+  
+  disagg_input <- rbind(sample_data_1to9, sample_data_18to24, sample_data_25_plus)
+  # I can dput the binded rows but creating them separately is more explanatory.
+  
+  agg_output <- TransformAnalyticsOutput_SiteTool(disagg_input, datapackcommons::dim_item_sets,
+                                                  data_element_map, 4)
+  
+  # Test to check that the value is being aggregated
+  testthat::expect_equal(agg_output[["aggregations"]]$Value, disagg_input$Value[2]*2)
+  
+  # Test to check that the right age option is being aggregated
+  testthat::expect_equal("Q6xWcyHDq6e", agg_output[["aggregations"]]$age_option_uid)
+  
+  # Asserts that all other rows have disagg values
+  non_agg_data <- agg_output[["processed"]] %>% dplyr::filter(age_option_uid != "Q6xWcyHDq6e")
+  testthat::expect_equal(non_agg_data$Value, rep(333/nrow(non_agg_data), times = (4)))
+  
+  # Making sure the age disaggs happen for input ages 1-9
+  testthat::expect_equal(non_agg_data$age_option_name, rep(c("5-9","1-4"), times = (2)))
 })
