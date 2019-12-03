1b2a36d10d51ee231b2d5cb072d5f9581ee164b3
sam-bao <sgarman@baosystems.com>
2019-12-03 16:51:25 +0100

    Update SnuxImDistMain.R
    Build Dimension List function now functional

--- R/SnuxImDistMain.R
+++ R/SnuxImDistMain.R
@@ -1,17 +1,16 @@
-#' @title BuildDimensionLists(data_element_map_item, dim_item_sets, mechanisms, country_details)
+#' @title BuildDimensionList_SnuByIm(data_element_map_item, dim_item_sets, mechanisms, country_uid)
 #' 
-#' @description # get list of dimensions (parameters) for analytics call by level {planning, community, facility}
-#' analytics will be called seperatly for each level.
-#' Tightly coupled to DATIM as it contains some hard coded dimension uids for Funding Mechanism, technical area, 
-#' num or denom, disagg type, support type, and type of organization unit. Also some hard coded dimension items for
-#' type of organization unit and support type
+#' @description # get list of dimensions (parameters) for analytics call to get data for SNUxIM 
+#' distribution. Tightly coupled to DATIM as it contains some hard coded dimension uids 
+#' for Funding Mechanism, technical area, num or denom, disagg type, support type, 
+#' and type of organization unit. Also some hard coded dimension items for support type
 #' @param data_element_map_item Single row of data_element_map being sliced and passed
 #' @param dim_item_sets Dataframe containing all the dimension item sets e.g. datapackcommons::dim_item_sets
 #' @param mechanisms All historic mechanisms for the country filtered by id
-#' @param country_details Country levels for a country
+#' @param country_uid Country uid
 #' @return  List of dimensions for the analytics call GetData_Analytics
-BuildDimensionLists_Mechanisms <- function(data_element_map_item, dim_item_sets, 
-                                mechanisms, country_details){
+BuildDimensionList_SnuByIm <- function(data_element_map_item, dim_item_sets, 
+                                       mechanisms, country_uid){
   # prepare df of dimensions and filters as expected by GetData_analytics  
   dimension_disaggs <- dim_item_sets %>% dplyr::mutate(type = "dimension") %>%  
     dplyr::filter(model_sets %in% c(data_element_map_item$age_set,
@@ -22,57 +21,25 @@ BuildDimensionLists_Mechanisms <- function(data_element_map_item, dim_item_sets,
     unique()  %>% 
     stats::na.omit() # there are some items in dim item sets with no source dimension
   
-  dimension_mechanisms <- mechanisms["categoryOptionId"] %>% dplyr::transmute(type = "dimension", 
-                                                                              dim_item_uid = categoryOptionId,
-                                                                              dim_uid = "SH885jaRe0o")
+  dimension_mechanisms <- mechanisms["categoryOptionId"] %>% 
+    dplyr::transmute(type = "dimension",
+                     dim_item_uid = categoryOptionId,
+                     dim_uid = "SH885jaRe0o")
   
-  # dimensions used by both numerator and denominator and prepped for GetData_Analytics
-  dimensions_common <- 
-    tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
-                    "filter", data_element_map_item[[1,"dx"]],"dx", 
-                    "filter", data_element_map_item[[1,"pe"]], "pe",
-                    "dimension", country_details$id, "ou",
-                    "dimension", data_element_map_item[[1,"technical_area_uid"]], "LxhLO68FcXm",
-                    "dimension", data_element_map_item[[1,"num_or_den_uid"]],"lD2x0c8kywj",
-                    "dimension", data_element_map_item[[1,"disagg_type_uid"]],"HWPJnUTMjEq") %>% 
+  # remaining dimensions
+  
+  tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
+                  "filter", data_element_map_item[[1,"dx"]],"dx", 
+                  "filter", data_element_map_item[[1,"pe"]], "pe",
+                  "dimension", country_uid, "ou",
+                  "dimension", "OU_GROUP-nwQbMeALRjL", "ou", # military
+                  "dimension", "OU_GROUP-AVy8gJXym2D", "ou", # COP Prioritization SNU
+                  "dimension", data_element_map_item[[1,"technical_area_uid"]], "LxhLO68FcXm",
+                  "dimension", data_element_map_item[[1,"num_or_den_uid"]],"lD2x0c8kywj",
+                  "dimension", data_element_map_item[[1,"disagg_type_uid"]],"HWPJnUTMjEq",
+                  "dimension", "iM13vdNLWKb", "TWXpUVE2MqL", #dsd and ta support types
+                  "dimension", "cRAGKdWIDn4", "TWXpUVE2MqL") %>% 
     dplyr::bind_rows(dimension_mechanisms, dimension_disaggs)
-  
-  dimensions_planning <- #filter on community and facility gets rid of military PSNU
-    tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
-                    "dimension", paste0("LEVEL-", country_details$planning_level), "ou") %>% 
-    dplyr::bind_rows(dimensions_common)
-  
-  out <- list(planning = dimensions_planning)
-return(out)
-  if(data_element_map_item$facility_valid == TRUE){
-    dimensions_facility <- 
-      tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
-                      "dimension", paste0("LEVEL-", country_details$facility_level), "ou",
-                      "dimension", "POHZmzofoVx", "mINJi7rR1a6", # facility org type
-                      "dimension", "iM13vdNLWKb", "TWXpUVE2MqL", #dsd and ta support types
-                      "dimension", "cRAGKdWIDn4", "TWXpUVE2MqL") %>% 
-      dplyr::bind_rows(dimensions_common)
-    
-    out$planning <- rbind(out$planning, 
-                                 c("filter", "POHZmzofoVx", "mINJi7rR1a6"))
-    out$facility <-  dimensions_facility
-  }
-  
-  if(data_element_map_item$community_valid == TRUE){
-    dimensions_community <- 
-      tibble::tribble(~type, ~dim_item_uid, ~dim_uid,
-                      "dimension", paste0("LEVEL-", country_details$community_level), "ou",
-                      "dimension", "PvuaP6YALSA","mINJi7rR1a6", # community org type
-                      "dimension", "iM13vdNLWKb", "TWXpUVE2MqL", #dsd and ta support types
-                      "dimension", "cRAGKdWIDn4", "TWXpUVE2MqL") %>% 
-      dplyr::bind_rows(dimensions_common)
-    
-    out$planning <- rbind(out$planning, 
-                          c("filter", "PvuaP6YALSA", "mINJi7rR1a6"))
-    out$community = dimensions_community
-  }
-  
-return(out)
 }
 
 
@@ -86,7 +53,7 @@ base_url <- getOption("baseurl")
 # Get the mechanisms relevant for the specifc country being processed
 # cache options required for datimvalidation function to work.
 # cache age option reverts to original after calling datim validation
-country_details = datapackcommons::GetCountryLevels(base_url, "Rwanda")
+country_details = datapackcommons::GetCountryLevels(base_url, "Malawi")
 
 cache_in = getOption("maxCacheAge")
 options(maxCacheAge = 0)
@@ -118,8 +85,10 @@ mechanisms_historic_country <- mechanisms_historic_global %>%
 
 
 
-temp=BuildDimensionLists_Mechanisms(datapackcommons::Map19Tto20T %>% slice(1), 
+temp=BuildDimensionList_SnuByIm(datapackcommons::Map19Tto20T %>% slice(46), 
                                datapackcommons::dim_item_sets,
                                mechanisms_historic_country,
-                               datapackcommons::GetCountryLevels(base_url, "Rwanda"))
+                               datapackcommons::GetCountryLevels(base_url, 
+                                                                 "Malawi")[[1,"id"]])
 
+temp2=datapackcommons::GetData_Analytics(temp)
