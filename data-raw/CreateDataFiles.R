9ab2edc49773315e3ab374947dcce4c5e7a98867
sam-bao <sgarman@baosystems.com>
2019-12-19 12:08:53 +0100

    initial commit 20Tto21T rda

--- data-raw/CreateDataFiles.R
+++ data-raw/CreateDataFiles.R
@@ -83,19 +83,19 @@ ValidateDataRequired <- function(data_required, base_url){
   # data_required %>% ValidateDimItems("B.add_dim_1_uid", "B.add_dim_1_items", "B.add_dim_1_items_uid", base_url)
 }
 
-ValidateMap19Tto20T <- function(Map19Tto20T, dim_item_sets, base_url){
+ValidateMapT_1toT <- function(t_1_to_t, dim_item_sets, base_url){
   
-  Map19Tto20T %>% mutate(dim_uid_colname = "LxhLO68FcXm") %>% 
+  t_1_to_t %>% mutate(dim_uid_colname = "LxhLO68FcXm") %>% 
   ValidateDimItems("dim_uid_colname", "technical_area", "technical_area_uid", base_url) 
   
-  Map19Tto20T %>% mutate(dim_uid_colname = "lD2x0c8kywj") %>% 
+  t_1_to_t %>% mutate(dim_uid_colname = "lD2x0c8kywj") %>% 
     ValidateDimItems("dim_uid_colname", "num_or_den", "num_or_den_uid", base_url) 
       
-  Map19Tto20T %>% mutate(dim_uid_colname = "HWPJnUTMjEq") %>% 
+  t_1_to_t %>% mutate(dim_uid_colname = "HWPJnUTMjEq") %>% 
     ValidateDimItems("dim_uid_colname", "disagg_type", "disagg_type_uid", base_url) 
   
 # chack for matching model sets in Dimension item sets
-  c(Map19Tto20T$age_set, Map19Tto20T$sex_set, Map19Tto20T$kp_set, Map19Tto20T$other_disagg) %>% 
+  c(t_1_to_t$age_set, t_1_to_t$sex_set, t_1_to_t$kp_set, t_1_to_t$other_disagg) %>% 
     na.omit() %>% 
     {. %in% dim_item_sets$model_sets} %>% 
     all() %>% 
@@ -142,6 +142,12 @@ ValidateDataRequired(data_required, base_url)
 
 Map19Tto20T <- 
   readr::read_csv("./data-raw/site_distribution_configuration/19Tto20TMap.csv", 
+                  col_types = readr::cols(.default = "c"),
+                  na = c("NA")) 
+
+
+Map20Tto21T <- 
+  readr::read_csv("./data-raw/snu_x_im_distribution_configuration/20Tto21TMap.csv", 
                   col_types = readr::cols(.default = "c"),
                   na = c("NA")) 
 
@@ -208,7 +214,9 @@ Map19Tto20T <- mutate(Map19Tto20T,
                       facility_valid = as.logical(is_facility_indicator)
                       )
 
-ValidateMap19Tto20T(Map19Tto20T, dim_item_sets, base_url)
+ValidateMapT_1toT(Map19Tto20T, dim_item_sets, base_url)
+
+ValidateMapT_1toT(Map20Tto21T, dim_item_sets, base_url)
 
 
 
