4cde391b9b56343ad4c291485bb9c04f6f21b8e7
sam-bao <sgarman@baosystems.com>
2020-08-19 14:19:42 +0200

    Update SnuxImDistMain.R

--- data-raw/SnuxImDistMain.R
+++ data-raw/SnuxImDistMain.R
@@ -233,7 +233,7 @@ country_details <-  datapackcommons::GetCountryLevels(base_url)
 data <-  country_details[["id"]] %>% 
   purrr::map(process_country, mechs)
 data <- setNames(data,country_details$id)
-#readr::write_rds(data,"/Users/sam/COP data/PSNUxIM_20200207.rds", compress = c("gz"))
-data_old=readr::read_rds("/Users/sam/COP data/PSNUxIM_20200228.rds")
+# readr::write_rds(data,"/Users/sam/COP data/PSNUxIM_20200819.rds", compress = c("gz"))
+data_old=readr::read_rds("/Users/sam/COP data/PSNUxIM_20200729.rds")
 purrr::map(names(data), ~dplyr::all_equal(data[[.x]],data_old[[.x]])) %>% 
   setNames(country_details$country_name)
