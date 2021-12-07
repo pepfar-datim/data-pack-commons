27ef244197343b1da84e70f954c78760f0c20b07
sam-bao <sgarman@baosystems.com>
2021-12-07 13:14:34 +0100

    Update SnuxImDistMain.R

--- data-raw/SnuxImDistMain.R
+++ data-raw/SnuxImDistMain.R
@@ -283,6 +283,7 @@ data <-  country_details[["id"]] %>%
 
 data <- setNames(data,country_details$id)
 # readr::write_rds(data,"/Users/sam/COP data/PSNUxIM_202101206_3.rds", compress = c("gz"))
+# readr::write_rds(data,"/Users/sam/COP data/snuxim_model_data.rds", compress = c("gz"))
 data_old = readr::read_rds("/Users/sam/COP data/PSNUxIM_202101206_2.rds")
 data_old <- setNames(data_old,country_details$id)
 
