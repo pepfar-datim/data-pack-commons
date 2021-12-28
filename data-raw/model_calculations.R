98def94276ccd066aa09529a1f7c5639d03f8250
sam-bao <sgarman@baosystems.com>
2021-12-28 18:27:46 +0100

    Fix issues in datapack model with TX_PVLS

--- data-raw/model_calculations.R
+++ data-raw/model_calculations.R
@@ -191,7 +191,7 @@ output_location <- "/Users/sam/COP data/COP22 Update/"
 # get country and prioritization level
  operating_units <- datapackcommons::GetCountryLevels(base_url) %>%
    dplyr::arrange(country_name) %>% 
-    # filter(country_name == "Mozambique") %>% 
+        filter(country_name >= "Zim") %>% 
    dplyr::filter(prioritization_level != 0) # Turkmenistan has no planning/priortization level
  
  priority_snu_data <- 
@@ -274,8 +274,8 @@ for (ou_index in 1:NROW(operating_units)) {
   }
 
 print(lubridate::now())
-# saveRDS(flattenDataPackModel_21(cop_data), file = paste0(output_location,"model_data_pack_input_22_20211220_1_flat.rds"))
-# saveRDS(cop_data, file = paste0(output_location,"model_data_pack_input_22_20211220_1.rds"))
+# saveRDS(flattenDataPackModel_21(cop_data), file = paste0(output_location,"model_data_pack_input_22_20211281_1_flat.rds"))
+# saveRDS(cop_data, file = paste0(output_location,"model_data_pack_input_22_20211228_1.rds"))
 
 
 ### COMPARISAON CODE FOR TWO DIFFERENT OUTPUT FILES
@@ -288,7 +288,7 @@ print(lubridate::now())
 #                                    "Western_Africa_Data_Pack", "Western_Africa_Data_Pack")
 #
 # cop_data_new=cop_data
-# cop_data_old <- readRDS(file = paste0(output_location,"model_data_pack_input_22_20211130_1.rds"))
+# cop_data_old <- readRDS(file = paste0(output_location,"model_data_pack_input_22_20211228_1.rds"))
 # deltas = data.frame()
 # for (operating_unit in operating_units$id) {
 #   print(filter(operating_units, operating_units$id == operating_unit))
