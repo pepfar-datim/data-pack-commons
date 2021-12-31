8e8565fbc14f26dcd5f3e72fe72a71fe321eb50d
sam-bao <sgarman@baosystems.com>
2021-12-31 16:07:51 +0100

    updates to use d2_session object

--- tests/testthat/test-metadata-load.R
+++ tests/testthat/test-metadata-load.R
@@ -2,19 +2,19 @@ context("Read some metadata")
 
 with_mock_api({
 test_that("We can load data elements", {
-  test_login <- DHISLogin(test_config("test-config.json"))
+  play_230 <- list(base_url = "https://play.dhis2.org/2.30/",
+                   handle = httr::handle("https://play.dhis2.org/2.30/"))
   test_filters="name:like:HIV positive"
   test_fields=c("id","name","shortName")
   test_de_payload<-getMetadata("dataElements",
                                filters=test_filters,
                                fields=test_fields,
-                               "https://play.dhis2.org/2.30/")
+                               d2_session = play_230)
   expect_type(test_de_payload,"list")
   expect_setequal(names(test_de_payload),c(test_fields))
 }) })
 
 with_mock_api({
   test_that("We produce an error on a bad end point", {
-    test_login <- DHISLogin(test_config("test-config.json"))
     expect_error(getMetadata("foo"))
   }) })
