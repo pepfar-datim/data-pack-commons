6ab5ec01efbb54e158e8660590e81458708fb634
sam-bao <sgarman@baosystems.com>
2022-01-04 09:31:43 +0100

    Update SnuxImDistMain.R

--- data-raw/SnuxImDistMain.R
+++ data-raw/SnuxImDistMain.R
@@ -288,9 +288,9 @@ data <-  country_details[["id"]] %>%
 #data$ODOymOOWyl0 <- process_country("ODOymOOWyl0", mechs) 
 
 data <- setNames(data,country_details$id)
-# readr::write_rds(data,"/Users/sam/COP data/PSNUxIM_202101206_3.rds", compress = c("gz"))
+# readr::write_rds(data,"/Users/sam/COP data/PSNUxIM_20220104_1.rds", compress = c("gz"))
 # readr::write_rds(data,"/Users/sam/COP data/snuxim_model_data.rds", compress = c("gz"))
-data_old = readr::read_rds("/Users/sam/COP data/PSNUxIM_202101206_3.rds")
+data_old = readr::read_rds("/Users/sam/COP data/PSNUxIM20220104_1.rds")
 data_old <- setNames(data_old,country_details$id)
 
 non_nulls <- purrr::map_lgl(names(data), 
