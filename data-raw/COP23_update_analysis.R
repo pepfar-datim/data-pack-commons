80a29c7aecbe4b88337d1774d64109bb602222d1
Fausto Lopez <flopez@baosystems.com>
2023-02-10 15:39:31 -0500

    Update COP23_update_analysis.R
    - add in progress analysis

--- data-raw/COP23_update_analysis.R
+++ data-raw/COP23_update_analysis.R
@@ -41,3 +41,73 @@ View(joined)
 
 
 ### End DP-895
+
+
+### Begin DP-896
+
+# Get a comparison of the FY22 and FY21 results data to compare drift
+# MER R: Community Based FY2022Q4 - HfhTPdnRWES
+# MER R: Community Based FY2021Q4 - TBcmmtoaCBC
+
+comparison_community <- datapackcommons::diffDataEntryForm("HfhTPdnRWES",
+                                                 "TBcmmtoaCBC")
+
+# dataelement/catCombos in the newer but not older data entry form
+added <- dplyr::select(comparison_community$b_not_a, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("added" = "data")
+
+# dataelement/catCombos in the older but not newer data entry form
+removed <- dplyr::select(comparison_community$a_not_b, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("removed" = "data")
+
+# unchanged dataelement/catCombos
+stable <- dplyr::select(comparison_community$a_and_b, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("stable" = "data")
+
+# combined
+joined_community <- dplyr::full_join(added, removed) %>%
+  dplyr::full_join(stable) %>%
+  dplyr::filter(purrr::map2_lgl(added, removed,
+                                ~ !(is.null(.x) && is.null(.y))))
+
+
+
+
+
+# MER R: Facility Based FY2022Q4 - BHlhyPmRTUY
+# MER R: Facility Based FY2021Q4 - zL8TlPVzEBZ
+
+comparison_facility <- datapackcommons::diffDataEntryForm("BHlhyPmRTUY",
+                                                           "zL8TlPVzEBZ")
+
+# dataelement/catCombos in the newer but not older data entry form
+added <- dplyr::select(comparison_facility$b_not_a, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("added" = "data")
+
+# dataelement/catCombos in the older but not newer data entry form
+removed <- dplyr::select(comparison_facility$a_not_b, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("removed" = "data")
+
+# unchanged dataelement/catCombos
+stable <- dplyr::select(comparison_facility$a_and_b, dataelement, categoryoptioncombo) %>%
+  dplyr::group_by(dataelement) %>%
+  tidyr::nest() %>%
+  dplyr::rename("stable" = "data")
+
+# combined
+joined_community <- dplyr::full_join(added, removed) %>%
+  dplyr::full_join(stable) %>%
+  dplyr::filter(purrr::map2_lgl(added, removed,
+                                ~ !(is.null(.x) && is.null(.y))))
+
+### End DP-896
