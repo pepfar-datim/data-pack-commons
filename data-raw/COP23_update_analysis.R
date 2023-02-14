e3dd1c59445e5cb03c4f1f51fbfd6d17b3a07aa5
Fausto Lopez <flopez@baosystems.com>
2023-02-13 21:24:41 -0500

    Update COP23_update_analysis.R
    - add in more analysis points for DP-896

--- data-raw/COP23_update_analysis.R
+++ data-raw/COP23_update_analysis.R
@@ -49,8 +49,8 @@ View(joined)
 # MER R: Community Based FY2022Q4 - HfhTPdnRWES
 # MER R: Community Based FY2021Q4 - TBcmmtoaCBC
 
-comparison_community <- datapackcommons::diffDataEntryForm("HfhTPdnRWES",
-                                                 "TBcmmtoaCBC")
+comparison_community <- datapackcommons::diffDataEntryForm("TBcmmtoaCBC",
+                                                          "HfhTPdnRWES")
 
 # dataelement/catCombos in the newer but not older data entry form
 added <- dplyr::select(comparison_community$b_not_a, dataelement, categoryoptioncombo) %>%
@@ -75,13 +75,13 @@ joined_community <- dplyr::full_join(added, removed) %>%
   dplyr::full_join(stable) %>%
   dplyr::filter(purrr::map2_lgl(added, removed,
                                 ~ !(is.null(.x) && is.null(.y)))) %>%
-  dplyr::mutate(type = "comunity")
+  dplyr::mutate(type = "community")
 
 # MER R: Facility Based FY2022Q4 - BHlhyPmRTUY
 # MER R: Facility Based FY2021Q4 - zL8TlPVzEBZ
 
-comparison_facility <- datapackcommons::diffDataEntryForm("BHlhyPmRTUY",
-                                                           "zL8TlPVzEBZ")
+comparison_facility <- datapackcommons::diffDataEntryForm("zL8TlPVzEBZ",
+                                                           "BHlhyPmRTUY")
 
 # dataelement/catCombos in the newer but not older data entry form
 added <- dplyr::select(comparison_facility$b_not_a, dataelement, categoryoptioncombo) %>%
@@ -116,12 +116,36 @@ joined_all <- rbind(
 )
 
 # overlapped items
-overlap <- joined_all[duplicated(joined_all$dataelement),]$dataelement
-joined_all_overlap <- joined_all[joined_all$dataelement %in% overlap,] %>%
+overlap <- joined_all[duplicated(joined_all$dataelement), ]$dataelement
+joined_all_overlap <- joined_all[joined_all$dataelement %in% overlap, ] %>%
   dplyr::arrange(dataelement)
 
 # unique records
-joined_all_uniq <- joined_all[!duplicated(joined_all$dataelement),]
+joined_all_uniq <- joined_all[!duplicated(joined_all$dataelement), ]
 
+# review priority indicators (currently nonhighlighted indicators)
+# PMTCT is in facility
+unique(comparison_facility$a_and_b$dataelement)[grep("PMTCT_", unique(comparison_facility$a_and_b$dataelement))]
+unique(comparison_community$a_and_b$dataelement)[grep("PMTCT_", unique(comparison_community$a_and_b$dataelement))]
+
+#VMMC
+unique(comparison_facility$a_and_b$dataelement)[grep("VMMC", unique(comparison_facility$a_and_b$dataelement))]
+unique(comparison_community$a_and_b$dataelement)[grep("VMMC", unique(comparison_community$a_and_b$dataelement))]
+
+#TX_TB
+unique(comparison_facility$a_and_b$dataelement)[grep("TX_TB", unique(comparison_facility$a_and_b$dataelement))]
+unique(comparison_community$a_and_b$dataelement)[grep("TX_TB", unique(comparison_community$a_and_b$dataelement))]
+
+#PREP_NEW
+unique(comparison_facility$a_and_b$dataelement)[grep("PrEP_NEW", unique(comparison_facility$a_and_b$dataelement))]
+unique(comparison_community$a_and_b$dataelement)[grep("PrEP_NEW", unique(comparison_community$a_and_b$dataelement))]
+
+#TX_CURR
+unique(comparison_facility$a_and_b$dataelement)[grep("TX_CURR", unique(comparison_facility$a_and_b$dataelement))]
+unique(comparison_community$a_and_b$dataelement)[grep("TX_CURR", unique(comparison_community$a_and_b$dataelement))]
+
+
+# write out if needed
+# write.csv(joined_all_uniq[,c("dataelement","type")], "dp-896-unique-all-dataelements-diff.csv", row.names = F)
 
 ### End DP-896
